7678418e4aab18ac4f81f8959ad8a950a82ab7d0
muhammed rifan <rifanmuhammed173@gmail.com>
2023-10-08 22:02:00 +0530

    home page starts

--- src/app/User/user.module.ts
+++ src/app/User/user.module.ts
@@ -3,14 +3,15 @@ import {NgModule} from "@angular/core"
 import { UserRoutingModule } from "./user-routing.module"
 import {MatButtonModule} from "@angular/material/button"
 import { LoginComponent } from "./components/login/login.component"
-import { RegisterComponent } from "./components/register/register.component"
+import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from './components/home/home.component'
 
 
 
 @NgModule({
     declarations:[
         LoginComponent, 
-        RegisterComponent
+        RegisterComponent, HomeComponent
     ],
     imports:[
         CommonModule,
